7c2910c31f006425d3a7298a1e2ca7d9832a9209
Uros Nesic <unesi98@gmail.com>
2021-03-18 16:08:49 +0100

    added router

---
+++ src/views/Home.tsx
@@ -0,0 +1,11 @@
+import React from "react";
+
+interface HomeProps {}
+
+export const Home: React.FC<HomeProps> = () => {
+	return (
+		<div className="App">
+			<h1 className="App__Title">👋🏻Henlo fren!👋🏻</h1>
+		</div>
+	);
+};
